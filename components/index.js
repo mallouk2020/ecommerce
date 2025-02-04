ae794c537a539572ca7ace2351f3a5a66db96153
mallouk2020 <elyassmallouk@gmail.com>
2025-02-04 16:09:39 +0100

    Update index.js
    thets for vercal error

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export {default as Footer}  from './footer';
 export {default as layout}  from './layout';
-export {default as navbar}  from './navbar';
+export {default as navbar}  from './Navbar';
 export {default as Product}  from './product';
 export {default as HeroBanner}  from './heroBanner';
 export {default as Footerbanner}  from './footerbanner';
